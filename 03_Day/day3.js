e2a063384950c56370624a527c6cfbe83d792ba1
Jeevan Pandey-dev <jeevankpandey123@gmail.com>
2025-03-20 22:04:14 +0530

    Working on day3

--- 03_Day/day3.js
+++ 03_Day/day3.js
@@ -1,14 +1,37 @@
 // Activity 1: If-Else Statements
 
-// Task 1: Write a program to check if a number is positive, negative, or zero, and log the result to the console. Task 2: Write a program to check if a person is eligible to vote (age 18) and log the result to the console.
+// Task 1: Write a program to check if a number is positive, negative, or zero, and log the result to the console. 
 
+function checkingForNum(number) {
+    if (number > 0) {
+        return console.log(`${number} is Positive Number`);
+    } else if (number < 0) {
+        return console.log(`${number} is Negative Number`);
+    } else if (number == 0) {
+        return console.log(`${number} is Equal to Zero`); 
+    } else {
+        return console.log(`${number} is Not A Number`);
+    }
+}
+checkingForNum("abc") // Output: abc Not A Number
+
+// Task 2: Write a program to check if a person is eligible to vote (age 18) and log the result to the console.
+
+const checkEligibilty = (age) => {
+    if (age >= 18) {
+        return "You Are Eligible to Vote"
+    } else {
+        return "Not Eligible to Vote"
+    }
+}
+
+console.log(checkEligibilty(5)); //Output : Not Eligible to Vote
 
 
 
 // Activity 2: Nested If-Else Statements
 
 // Task 3: Write a program to find the largest of three numbers using nested if-else statements.
-
 
 
 
@@ -31,3 +54,11 @@
 // Activity 5: Combining Conditions
 
 // Task 7: Write a program to check if a year is a leap year using multiple conditions (divisible by 4, but not 100 unless also divisible by 400) and log the result to the console.
+
+
+
+// Feature Request 1: Write a script that checks if a number is positive, negative, or zero using if-else statements and logs the result.
+
+
+
+// Feature Request 2: Create a script to check if a person is eligible to vote based on their age and log the result.
