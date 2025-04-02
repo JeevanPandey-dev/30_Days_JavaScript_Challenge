5a79d97f2c7022078d5bbf983ec3a885be6d036d
Jeevan Pandey-dev <jeevankpandey123@gmail.com>
2025-04-02 09:27:58 +0530

    Day4 Working

--- 04_Day/day4.js
+++ 04_Day/day4.js
@@ -3,14 +3,14 @@
 // Task 1 Write a program to print numbers from 1 to 10 using a for loop.
 
 for (let i = 1; i <= 10; i++) {
-    console.log(i); 
+    // console.log(i); 
 }
 
 
 // Task 2 Write a program to print the multiplication table of 5 using a for loop.
-console.log("Table of 5");
+// console.log("Table of 5");
 for (let i = 1; i <= 10; i++) {
-    console.log(`5 x ${i} = ${5 * i}`);
+    // console.log(`5 x ${i} = ${5 * i}`);
 }
 
 
@@ -18,9 +18,23 @@ for (let i = 1; i <= 10; i++) {
 
 // Task 3 Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
 
+let sum = 0;
+let numBer = 1;
+while (numBer <= 10) {
+    sum += numBer;
+    numBer++;
+}
+console.log(`Sum of Number = ${sum} `);
+
+
 // Task 4 Write a program to print numbers from 10 to 1 using a while loop.
 
+let countNums = 10;
 
+while (num <= 1) {
+    num--;
+}
+console.log(`Numbers in Reverse Order ${num}`);
 
 
 // Activity 3: Do While Loop
