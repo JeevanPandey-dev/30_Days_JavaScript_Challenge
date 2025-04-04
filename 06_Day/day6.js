9e36c982099a98315323b7f5d8923ab68cabb777
Jeevan Pandey-dev <jeevankpandey123@gmail.com>
2025-04-04 18:08:55 +0530

    day6...Working

--- 06_Day/day6.js
+++ 06_Day/day6.js
@@ -2,17 +2,38 @@
 
 // Task 1 Create an array of numbers from 1 to 5 and log the array to the console.
 
+const NumArr = [1,2,3,4,5]
+
+console.log(NumArr);
+
 // Task 2 Access the first and last elements of the array and log them to the console.
+
+const newArr = ["Aarav","Neeraj","Fezu","Manvik"]
+
+console.log(newArr[0], newArr.at(-1));
 
 //  Activity 2: Array Methods (Basic)
 
-// Task 3 Use the `push` method to add a new number to the end of the array and log the updated array.
+// Task 3 Use the `push` method to add a new member to the end of the array and log the updated array.
+
+newArr.push("Harsha")
+
+console.log(newArr);
 
 // Task 4 Use the `pop` method to remove the last element from the array and log the updated array.
 
+newArr.pop()
+console.log(newArr);
+
 // Task 5 Use the `shift` method to remove the first element from the array and log the updated array.
 
-// Task 6 Use the `unshift` method to add a new number to the beginning of the array and log the updated array.
+newArr.shift()
+console.log(newArr);
+
+// Task 6 Use the `unshift` method to add a new element to the beginning of the array and log the updated array.
+
+newArr.unshift('Harsha')
+console.log(newArr);
 
 //  Activity 3: Array Methods (Intermediate)
 
@@ -33,6 +54,8 @@
 // Task 12 Create a two-dimensional array (matrix) and log the entire array to the console.
 
 // Task 13 Access and log a specific element from the two-dimensional array.
+
+
 //  Feature Requests
 
 // Array Manipulation Script Write a script that demonstrates the creation of an array, adding and removing elements using **push**, **pop**, **shift**, and **unshift** methods.
