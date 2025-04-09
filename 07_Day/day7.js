04f7e95ecbe367177f2a8f69d6e139af52888260
Jeevan Pandey-dev <jeevankpandey123@gmail.com>
2025-04-09 12:42:33 +0530

    Working on day7

--- 07_Day/day7.js
+++ 07_Day/day7.js
@@ -1,12 +1,24 @@
 // Task 1: Create an object representing a book with properties like title, author, and year, and log the object to the console.
 
+const bookProp = {
+    title : "The Subtle Art of Not Giving a F*ck",
+    author : "Mark Manson",
+    year : 2016
+}
+console.log(bookProp);
+
 
 // Task 2: Access and log the title and author properties of the book object.
 
-
+console.log(bookProp.title);
+console.log(bookProp.author);
 
 // Task 3: Add a method to the book object that returns a string with the book's title and author, and log the result of calling this method.
 
+bookProp.getProp = function(){
+    return `${this.title} by ${this.author}`
+}
+console.log(bookProp.getProp());
 
 
 // Task 4: Add a method to the book object that takes a parameter (year) and updates the book's year property, then log the updated object.
